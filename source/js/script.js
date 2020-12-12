379ac6771a0f963411a07f8f94c00a10c9485d2f
Aznaur <aznaur.khapchaev@gmail.com>
2020-12-12 13:25:04 +0300

    Реализовал открытие меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,43 +1,58 @@
 'use strict';
-{
-  let control = document.querySelector('.slider__control');
-  let items = document.querySelectorAll('.catalog__item');
+let openMenu = document.querySelector('.page-header__open');
+let mainNav = document.querySelector('.main-nav');
+let logo = document.querySelector('.page-header__logo');
+let closeMenu = document.querySelector('.main-nav__close');
 
-  if (screen.width <= 767) {
-    control.addEventListener('click', function (event) {
-      let target = event.target;
+openMenu.addEventListener('click', function () {
+  mainNav.classList.add('main-nav__toggle');
+  logo.classList.add('page-header__logo--menu');
+});
 
-      for (let itm of control.children) {
-        itm.classList.remove('slider__item--active');
-      }
+closeMenu.addEventListener('click', function () {
+  mainNav.classList.remove('main-nav__toggle');
+  logo.classList.remove('page-header__logo--menu');
+});
 
-      if (target.classList.contains('slider__item')) {
-        for (let key of items) {
-          key.classList.remove('catalog__item--active');
-        }
-        target.classList.add('slider__item--active');
-        items[+target.dataset.numb].classList.add('catalog__item--active');
-      }
-    });
+// {
+//   let control = document.querySelector('.slider__control');
+//   let items = document.querySelectorAll('.catalog__item');
 
-    console.log(320);
-  } else if (screen.width >= 768 && screen.width <= 1149 ) {
-    control.addEventListener('click', function (event) {
-      let target = event.target;
+//   if (screen.width <= 767) {
+//     control.addEventListener('click', function (event) {
+//       let target = event.target;
 
-      for (let itm of control.children) {
-        itm.classList.remove('slider__item--active');
-      }
+//       for (let itm of control.children) {
+//         itm.classList.remove('slider__item--active');
+//       }
 
-      if (target.classList.contains('slider__item')) {
-        for (let key of items) {
-          key.classList.remove('catalog__item--active');
-        }
-        target.classList.add('slider__item--active');
-        items[+target.dataset.numb].classList.add('catalog__item--active');
-        items[+target.dataset.numb + 1].classList.add('catalog__item--active');
-      }
-    });
-  }
+//       if (target.classList.contains('slider__item')) {
+//         for (let key of items) {
+//           key.classList.remove('catalog__item--active');
+//         }
+//         target.classList.add('slider__item--active');
+//         items[+target.dataset.numb].classList.add('catalog__item--active');
+//       }
+//     });
 
-}
+//     console.log(320);
+//   } else if (screen.width >= 768 && screen.width <= 1149 ) {
+//     control.addEventListener('click', function (event) {
+//       let target = event.target;
+
+//       for (let itm of control.children) {
+//         itm.classList.remove('slider__item--active');
+//       }
+
+//       if (target.classList.contains('slider__item')) {
+//         for (let key of items) {
+//           key.classList.remove('catalog__item--active');
+//         }
+//         target.classList.add('slider__item--active');
+//         items[+target.dataset.numb].classList.add('catalog__item--active');
+//         items[+target.dataset.numb + 1].classList.add('catalog__item--active');
+//       }
+//     });
+//   }
+
+// }
